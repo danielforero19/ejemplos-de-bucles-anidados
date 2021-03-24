95dbc8990fc6deeca775472c9e83ada3caff03e4
danielforero19 <alvarezdanielf@gmail.com>
2021-03-24 16:47:46 -0500

    ejemplos bucles anidados

---
+++ ejemplo 1.py
@@ -0,0 +1,3 @@
+for x in range(12):
+    for y in range(3):
+        print("x vale", x,"y vale", y)
---
+++ ejemplo 2.py
@@ -0,0 +1,3 @@
+for k in range(4):
+    for u in range(6):
+        print("k vale", k,"u vale", u)
